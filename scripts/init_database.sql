19bf2a3b8dff0945c989169069100aca6aa25d4a
sreejit-chatterjee <68802464+sreejit-chatterjee@users.noreply.github.com>
2025-05-22 12:39:43 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,32 @@
+-- Create Database  'DataWarehouse'
 
+USE master;
+GO
+
+
+--Drop and recreate the 'DataWareHouse'  database
+
+IF EXISTS (SELECT 1 from sys.databases WHERE name = 'DataWareHouse')
+BEGIN
+ ALTER DATABASE DataWareHouse SET SINGLE_USER  WITH ROLLBACK IMMEDIATE;
+ DROP DATABASE DataWareHouse;
+END
+GO
+
+-- Create database
+CREATE DATABASE DataWareHouse;
+GO 
+
+
+-- USe Database
+USE DataWareHouse;
+GO
+
+
+  -- Create Layer Schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
